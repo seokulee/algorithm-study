78ee94f1842a3c231ad3fbe17b090f309f969f16
kyungwon <power16one5@naver.com>
2024-05-16 17:00:18 +0900

    submit w06 9251 LCS

---
+++ parkkyungwon/w06/9251 LCS.py
@@ -0,0 +1,24 @@
+import sys
+
+
+def lcs(A, B):
+    m = len(A)
+    n = len(B)
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
+
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if A[i - 1] == B[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] + 1
+            else:
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+    
+    return dp[m][n]
+
+
+readline = sys.stdin.readline
+
+A = readline().strip()
+B = readline().strip()
+
+sys.stdout.write(str(lcs(A, B)) + '\n')
