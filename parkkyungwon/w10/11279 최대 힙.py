c8e06a309c4f3f9cada6f0cf883a3b8c9a5aa90d
kyungwon <power16one5@naver.com>
2024-06-11 11:07:04 +0900

    submit w10 11279 최대 힙

---
+++ parkkyungwon/w10/11279 최대 힙.py
@@ -0,0 +1,20 @@
+import sys
+import heapq
+
+
+readline = sys.stdin.readline
+heap = []
+
+for _ in range(int(readline())):
+    n = int(readline())
+
+    if n == 0:
+        if len(heap) == 0:
+            r = 0
+        else:
+            r = -heapq.heappop(heap)
+
+        sys.stdout.write(str(r) + '\n')
+            
+    else:
+        heapq.heappush(heap, -n)
