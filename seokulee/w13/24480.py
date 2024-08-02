a32fe2990bdb273bbeff04724ae6769396faf5b2
Seokkyu Lee <cd1004s@gmail.com>
2024-08-02 15:51:17 +0900

    submit w13 24480 알고리즘 수업 - dfs2

---
+++ seokulee/w13/24480.py
@@ -0,0 +1,28 @@
+import sys
+sys.setrecursionlimit(10**6)
+
+order = 1
+
+def dfs(vertex, visited, R):
+    global order
+    visited[R] = order
+    order += 1
+
+    vertex[R].sort(reverse=True)
+    for i in vertex[R]:
+        if not visited[i]:
+            dfs(vertex, visited, i)
+
+N, M, R = map(int, sys.stdin.readline().split())
+vertex = [[] for _ in range(N+1)]
+visited = [0] * (N+1)
+
+for i in range(M):
+    u, v = map(int, sys.stdin.readline().split())
+    vertex[u].append(v)
+    vertex[v].append(u)
+
+dfs(vertex, visited, R)
+
+for i in range(1, N+1):
+    print(visited[i])
