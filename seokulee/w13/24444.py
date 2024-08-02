ec592e317908fb81f4e69d8fc87ae964d0f36c1a
Seokkyu Lee <cd1004s@gmail.com>
2024-08-02 16:34:25 +0900

    submit w13 24444 알고리즘 수업 - bfs1

---
+++ seokulee/w13/24444.py
@@ -0,0 +1,39 @@
+import sys
+from collections import deque
+sys.setrecursionlimit(10**6)
+
+order = 1
+q = deque()
+
+def dfs(vertex, visited, R):
+    for v in range(N+1):
+        if v != R:
+            visited[v] = 0
+    global order
+    visited[R] = order
+    order += 1
+
+    q.append(R)
+
+    while q:
+        u = q.popleft()
+        vertex[u].sort()
+        for v in vertex[u]:
+            if not visited[v]:
+                visited[v] = order
+                order += 1
+                q.append(v)
+
+N, M, R = map(int, sys.stdin.readline().split())
+vertex = [[] for _ in range(N+1)]
+visited = [0] * (N+1)
+
+for i in range(M):
+    u, v = map(int, sys.stdin.readline().split())
+    vertex[u].append(v)
+    vertex[v].append(u)
+
+dfs(vertex, visited, R)
+
+for i in range(1, N+1):
+    print(visited[i])
