c1b309f6da2c5649b754b6d266d16d7e9da9a0c8
kyungwon <power16one5@naver.com>
2024-09-01 21:21:33 +0900

    refact w17 12852 1로 만들기 2

--- parkkyungwon/w17/12852 1로 만들기 2.py
+++ parkkyungwon/w17/12852 1로 만들기 2.py
@@ -1,30 +1,65 @@
+def forward(i):
+    for a in i+1, 2*i, 3*i:
+        if a >= L: return
+        yield a
+
+
+def backward(i):
+    if i % 3 == 0: yield i // 3
+    if i % 2 == 0: yield i // 2
+    yield i - 1
+
+
 def sol(n):
-    dp = [float('inf')] * (n+1)
-    dp[1] = 0
+    if n == 1:
+        return 0, [1]
 
-    for i in range(1, n):
-        v = dp[i] + 1
-        for j in i*3, i*2, i+1:
-            if j <= n and v < dp[j]:
-                dp[j] = v
-    
-    # 도착지에서 출발지로 역순으로 경로를 찾는다
-    count = dp[n] - 1
-    answer = [n]
-    i = n
+    dp = [None] * (L)
 
-    for _ in range(dp[n]):
-        for a, b in zip((i % 3 == 0, i % 2 == 0, True), (i // 3, i // 2, i - 1)):
-            if a and dp[b] == count:
-                answer.append(b)
-                count -= 1
-                i = b
-                break
+    # 투 포인터, bfs
+    def search():
+        dp[1], dp[n] = 0, -1
+        queue_for, queue_bac = [1], [n]
+
+        for s_side, e_side in zip(range(1, L), range(-2, -L, -1)):
+            for queue, gen, is_for in zip((queue_for, queue_bac), (forward, backward), (True, False)):
+                queue_tem = []
+
+                for q in queue:
+                    for k in gen(q):
+                        if dp[k] == None:
+                            dp[k] = s_side if is_for else e_side
+                            queue_tem.append(k)
+                        
+                        elif is_for:
+                            if dp[k] < 0: return s_side-1, e_side+2, k
+                        else:
+                            if dp[k] >= 0: return s_side-1, e_side+1, k
+                
+                if is_for: queue_for = queue_tem
+                else: queue_bac = queue_tem
     
-    return dp[n], answer
+    s_side, e_side, m = search()
+    
+    # 중앙에서 출발지와 도착지로 경로를 찾는다
+    leng = s_side - e_side + 2
+    answer = [None] * leng
+    answer[leng//2] = m
+
+    for s, e, op, gen in zip((s_side, e_side), (-1, 0), (-1, 1), (backward, forward)):
+        a = m
+        for i in range(s, e, op):
+            for j in gen(a):
+                if dp[j] == i:
+                    answer[i] = j
+                    a = j
+                    break
+
+    return leng-1, answer[::-1]
         
 
 N = int(input())
+L = N + 1
 
 leng, path = sol(N)
 
