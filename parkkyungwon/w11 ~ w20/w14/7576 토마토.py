c8fccce6e49ca06fd917e6a3fad8909d41bf531d
kyungwon <power16one5@naver.com>
2024-09-04 15:42:00 +0900

    fix w14 7576 토마토 가독성 향상을 위해 코드 정리

--- parkkyungwon/w11 ~ w20/w14/7576 토마토.py
+++ parkkyungwon/w11 ~ w20/w14/7576 토마토.py
@@ -2,53 +2,37 @@ import sys
 
 
 
-def sol(arr, m):
-    m += 2
-    # 상자 테두리에 -1을 추가하고 상자를 1차원 배열로 저장
-    box = [-1] * (m)
-    for a in arr:
-        box += [-1] + a + [-1]
-    box += [-1] * (m)
-
-    queue = []
+def sol(box, leng):
     # 익은 토마토를 찾고 queue에 넣음
-    for i in range(len(box)):
-        if box[i] == 1:
-            queue.append(i)
-    # bfs depth의 끝을 구분하기 위해 -1을 만나면 해당 depth의 끝
-    queue.append(-1)
-
-    count = 0
-    for a in queue:
-        if a == -1:
-            # queue내부에 -1 뒤로 다른것이 추가되지 않았다면 끝
-            if queue[-1] == -1:
-                break
-
-            queue.append(-1)
-            count += 1
-            continue
-
-        # 좌, 우, 상, 하 인덱스 확인
-        for b in a-1, a+1, a+m, a-m:
-            if box[b]:
-                continue
-
-            box[b] = 1
-            queue.append(b)
-
-    for a in box:
-        # box에 0이 존재하면 -1
-        if not a:
-            return -1
+    queue = [i for i in range(len(box)) if box[i] == 1]
+    count = -1
+            
+    while queue:
+        queue_tmp = []
         
-    else:
-        return count 
+        for q in queue:
+            # 좌, 우, 상, 하 인덱스 확인
+            for i in q-1, q+1, q+leng, q-leng:
+                if box[i]: continue
+
+                box[i] = 1
+                queue_tmp.append(i)
+        
+        count += 1
+        queue = queue_tmp
+
+    # box에 0이 존재하면 -1
+    return count if all(box) else -1
 
 
 readline = sys.stdin.readline
 M, N = map(int, readline().split())
+L = M + 2
 
-arr = [list(map(int, readline().split())) for _ in range(N)]
+# 상자 테두리에 -1을 추가하고 상자를 1차원 배열로 저장
+box = [-1] * L
+for a in (list(map(int, readline().split())) for _ in range(N)):
+    box += [-1] + a + [-1]
+box += [-1] * L
 
-print(sol(arr, M))
+print(sol(box, L))
