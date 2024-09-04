d39b42f890bbdf2271c1edbf52ce23f15eead557
kyungwon <power16one5@naver.com>
2024-09-04 14:09:41 +0900

    fix w18 5639 이진 검색 트리 주석 추가

--- parkkyungwon/w11 ~ w20/w18/5639 이진 검색 트리.py
+++ parkkyungwon/w11 ~ w20/w18/5639 이진 검색 트리.py
@@ -15,6 +15,8 @@ def sol(s, e, complement):
     v = nodes[s]
     answer[e-complement] = v
     complement += 1
+    # 루트를 사용해서 왼쪽은 모든 값이 루트보다 작고, 오른쪽은 모든 값이 크기 때문에 이진탐색이 가능
+    # 이진 탐색이 완료되면 좌, 우 서브트리 내에서 다시 루트를 사용해서 이진탐색
     sep = bisect.bisect_left(nodes, v, lo=s+1, hi=e)
     sol(s+1, sep, complement), sol(sep, e, complement)
 
