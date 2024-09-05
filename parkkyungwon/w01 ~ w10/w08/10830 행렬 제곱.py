bd826339526a6b7ce69c331f346133349485935e
kyungwon <power16one5@naver.com>
2024-09-05 17:01:53 +0900

    refact w08 10830 행렬 제곱 가독성을 높히고 주석 추가

--- parkkyungwon/w01 ~ w10/w08/10830 행렬 제곱.py
+++ parkkyungwon/w01 ~ w10/w08/10830 행렬 제곱.py
@@ -2,32 +2,30 @@ import sys
 
 
 
-readline = sys.stdin.readline
-tu = tuple
-
-
 def power(arr, ex, M):
-    arr = tu(tu(a % M for a in ar) for ar in arr)
-    arr_T = tu(v for v in zip(*arr))
+    arr = [[a % M for a in ar] for ar in arr]
+    arr_T = [ar for ar in zip(*arr)]
     
     def p(ex):
         if ex == 1:
             return arr
         
+        # 지수의 절반
         A = p(ex//2)
-        B = tu(v for v in zip(*A))
-        C = tu(tu(sum(x*y for x, y in zip(a, b)) % M for b in B) for a in A)
+        # 행렬 곱을 쉽게 하기위해 A의 전치 행렬을 구함
+        B = [a for a in zip(*A)]
+        # A행렬 제곱
+        C = [[sum(x*y for x, y in zip(a, b)) % M for b in B] for a in A]
 
-        return C if ex % 2 == 0 else tu(tu(sum(x*y % M for x, y in zip(a, b)) % M for b in arr_T) for a in C)
-
+        # 지수가 홀수면 C에 전치행렬 arr_T를 추가로 곱해줌
+        return C if ex % 2 == 0 else [[sum(x*y % M for x, y in zip(a, b)) % M for b in arr_T] for a in C]
 
     return p(ex)
 
 
+readline = sys.stdin.readline
 N, B = map(int, readline().split())
-arr = tu(tu(map(int, readline().split())) for _ in range(N))
+arr = [tuple(map(int, readline().split())) for _ in range(N)]
 
 for ar in power(arr, B, 1000):
-    for a in ar:
-        sys.stdout.write(str(a) + ' ')
-    sys.stdout.write('\n')
+    sys.stdout.write(' '.join(map(str, ar)) + '\n')
