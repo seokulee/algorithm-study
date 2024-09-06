7326445a5717bff35bde6dce5f9d9dda1a140b53
kyungwon <power16one5@naver.com>
2024-09-07 07:11:47 +0900

    fix w11 1520 내리막 길 과거엔 맞췄던 답이 현재 recursion error 오답 처리됌 recursion limit 증가 시킴

--- parkkyungwon/w11 ~ w20/w11/1520 내리막 길.py
+++ parkkyungwon/w11 ~ w20/w11/1520 내리막 길.py
@@ -2,6 +2,9 @@ import sys
 
 
 
+sys.setrecursionlimit(10**6)
+
+
 def find(arr, V, H):
     dp = [[-1] * (H+2) for _ in range(V+2)]
     dp[-2][-2] = 1
@@ -13,8 +16,7 @@ def find(arr, V, H):
         for dy, dx in directions:
             y2, x2 = y + dy, x + dx
 
-            if arr[y][x] <= arr[y2][x2]:
-                continue
+            if arr[y][x] <= arr[y2][x2]: continue
 
             if dp[y2][x2] == -1:
                 dp[y2][x2] = 0
@@ -22,22 +24,20 @@ def find(arr, V, H):
 
             dp[y][x] += dp[y2][x2]
 
-
     f(1, 1)
 
     return dp[1][1]
     
-
 
 readline = sys.stdin.readline
 V, H = map(int, readline().split())
 
 max_h = 10000
 
-outline = ((max_h, ) * (H+2), )
+outline = [(max_h, ) * (H+2)]
 arr = outline
 for _ in range(V):
-    arr += ((max_h, *(map(int, readline().split())), max_h), )
+    arr += [[max_h, *(map(int, readline().split())), max_h]]
 arr += outline
 
 print(find(arr, V, H))
