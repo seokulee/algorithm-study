501e40bf2c9c4a51e6d60a871cad5fbf1c6cf734
kyungwon <power16one5@naver.com>
2024-09-06 10:23:29 +0900

    refact w02 14425 문자열 집합 재채점 기념 코드 개선 input을 readline으로 변경

--- parkkyungwon/w01 ~ w10/w02/14425 문자열 집합.py
+++ parkkyungwon/w01 ~ w10/w02/14425 문자열 집합.py
@@ -1,10 +1,14 @@
-m_count, c_count = map(int, input().split())
+import sys
 
-m_set = {input() for _ in range(m_count)}
+
+
+readline = sys.stdin.readline
+m_count, c_count = map(int, readline().split())
+
+m_set = {readline() for _ in range(m_count)}
 
 included_count = 0
 for _ in range(c_count):
-    if input() in m_set:
-        included_count += 1
+    if readline() in m_set: included_count += 1
 
 print(included_count)
