6e9f8558e8a6eb190fd8d54370fcd01027a66e6f
kyungwon <power16one5@naver.com>
2024-09-07 09:55:12 +0900

    refact w11 11049 행렬 곱셈 순서 코드 개선

--- parkkyungwon/w11 ~ w20/w11/11049 행렬 곱셈 순서.py
+++ parkkyungwon/w11 ~ w20/w11/11049 행렬 곱셈 순서.py
@@ -2,30 +2,22 @@ import sys
 
 
 
-def find(arr):
-    L = len(arr) - 1
-    dp = [[0] * L for _ in range(L)]
-
-    p = [[0] * (L+1) for _ in range(L+1)]
-    for s in range(L):
-        for e in range(s+1, L+1):
-            p[s][e] = arr[s] * arr[e]
-
-    for leng in range(1, L):
-        for s in range(L - leng):
-            e = s + leng
-            dp[s][e] = float('inf')
-            for m in range(s, e):
-                cost = p[s][e+1] * arr[m+1] + dp[s][m] + dp[m+1][e]
-                if dp[s][e] > cost:
-                    dp[s][e] = cost
-            
-    return dp[0][-1]
-
-
 readline = sys.stdin.readline
 T = int(readline())
 arr = tuple(map(int, readline().split()))
 arr += tuple(int(readline().split()[1]) for _ in range(T-1))
 
-print(find(arr))
+def sol(arr):
+    L = len(arr)
+    dp = [[0] * (L) for _ in range(L)]
+
+    for leng in range(2, L):
+        for s in range(L-leng):
+            e = s + leng
+            side = arr[s] * arr[e]
+            s1 = s + 1
+            dp[s][e] = dp[e][s] = min(side * a + b + c for a, b, c in zip(arr[s1:e], dp[s][s1:e], dp[e][s1:e]))
+            
+    return dp[0][-1]
+
+print(sol(arr))
