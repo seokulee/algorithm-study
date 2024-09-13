50151a1b3ce13a95ca206d38b8ebe6cfbcb6d5b1
kyungwon <power16one5@naver.com>
2024-09-13 10:50:34 +0900

    refact w20 4386 별자리 만들기 함수 단위로 나누고 주석 추가

--- parkkyungwon/w11 ~ w20/w20/4386 별자리 만들기.py
+++ parkkyungwon/w11 ~ w20/w20/4386 별자리 만들기.py
@@ -32,41 +32,53 @@ class UnionFind():
         return -self.dp[self.find(i)]
 
 
-def sol(coor, uf):
-    L = len(coor)
+def set_heapq(arr, uf):
+    L = len(arr)
     heap = []
 
     for i in range(1, L-1):
-        ay = coor[i][0]
-        ax = coor[i][1]
+        ay = arr[i][0]
+        ax = arr[i][1]
         group = uf.find(i)
 
         for j in range(i+1, L):
             # 이미 연결되어 있으면 패스
             if uf.find(j) == group: continue
 
-            d = (ay - coor[j][0])**2 + (ax - coor[j][1])**2
+            # 제곱근은 간선을 선택한 뒤 계산
+            d = (ay - arr[j][0])**2 + (ax - arr[j][1])**2
 
             heapq.heappush(heap, (d, i, j))
+    
+    return heap
 
+
+def get_mst(heap, uf, count):
     cost = 0.0
-    count = L - 1
 
-    # 간선을 오름차순으로 사용해서 사이클이 생기지 않고, n-1개의 간선을 사용
+    # 간선 가중치 오름차순으로 뽑고, 사이클이 생기지 않는 n-1개를 선택
     while 1:
-        v, i, j = heapq.heappop(heap)
+        d, i, j = heapq.heappop(heap)
 
         if uf.union(i, j):
-            cost += v**(0.5)
+            # 제곱근 계산
+            cost += d**(0.5)
 
             if uf.get_size(i) == count: break
     
     return cost
 
 
+def sol(arr, uf):
+    heap = set_heapq(arr, uf)
+    
+    return get_mst(heap, uf, len(arr) - 1)
+
+
 readline = sys.stdin.readline
 N, M = map(int, readline().split())
 
+# 인덱스 계산하기 쉽게 0 인덱스에 더미 추가
 coor = [0] + [tuple(map(int, readline().split())) for _ in range(N)]
 uf = UnionFind(N+1)
 
