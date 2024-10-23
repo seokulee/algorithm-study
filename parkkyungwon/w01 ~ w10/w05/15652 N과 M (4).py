c56ebe6e1964c211cec74edab625082719539b0d
kyungwon <power16one5@naver.com>
2024-10-23 09:51:07 +0900

    refact w05 15652 N과 M (4)

--- parkkyungwon/w01 ~ w10/w05/15652 N과 M (4).py
+++ parkkyungwon/w01 ~ w10/w05/15652 N과 M (4).py
@@ -1,9 +1,24 @@
 import sys
-import itertools
 
 
 
+def sol(n, m):
+    L = n + 1
+
+    def backtrack(arr, i, depth):
+        if depth == m:
+            write(' '.join(arr) + '\n')
+            return
+
+        for j in range(i, L):
+            arr.append(str(j))
+            backtrack(arr, j, depth+1)
+            arr.pop()
+
+    backtrack([], 1, 0)
+
+
+write = sys.stdout.write
 N, M = map(int, input().split())
 
-for i in itertools.combinations_with_replacement(range(1, N+1), M):
-    sys.stdout.write(' '.join(map(str, i)) + '\n')
+sol(N, M)
