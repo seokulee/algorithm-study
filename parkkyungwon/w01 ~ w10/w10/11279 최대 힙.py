3165c1a60b9dd19ff190f7f89130c39255e11ece
kyungwon <power16one5@naver.com>
2024-10-27 14:21:02 +0900

    refact w10 11279 최대 힙 heap 직접 구현

--- parkkyungwon/w01 ~ w10/w10/11279 최대 힙.py
+++ parkkyungwon/w01 ~ w10/w10/11279 최대 힙.py
@@ -1,20 +1,49 @@
 import sys
-import heapq
+
+
+
+class Heap():
+    def __init__(self):
+        self.dp = []
+    
+    def push(self, n):
+        self.dp.append(n)
+        child = len(self.dp) - 1
+
+        while child and self.dp[child] > self.dp[parent := (child-1)//2]:
+            self.dp[child], self.dp[parent] = self.dp[parent], self.dp[child]
+            child = parent
+
+    def pop(self):
+        leng = len(self.dp)
+        if not leng: return 0
+        elif leng == 1: return self.dp.pop()
+
+        ret = self.dp[0]
+        self.dp[0] = self.dp.pop()
+        leng -= 1
+        parent = 0
+
+        while 1:
+            child1 = 2*parent + 1
+            child2 = child1 + 1
+            largest = parent 
+            
+            if child1 < leng and self.dp[largest] < self.dp[child1]: largest = child1
+            if child2 < leng and self.dp[largest] < self.dp[child2]: largest = child2
+            if largest == parent: break
+
+            self.dp[parent], self.dp[largest] = self.dp[largest], self.dp[parent]
+            parent = largest
+
+        return ret
 
 
 readline = sys.stdin.readline
-heap = []
+heap = Heap()
 
 for _ in range(int(readline())):
     n = int(readline())
 
-    if n == 0:
-        if len(heap) == 0:
-            r = 0
-        else:
-            r = -heapq.heappop(heap)
-
-        sys.stdout.write(str(r) + '\n')
-            
-    else:
-        heapq.heappush(heap, -n)
+    if n: heap.push(n)   
+    else:sys.stdout.write(str(heap.pop()) + '\n')
